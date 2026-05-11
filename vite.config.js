113c50e14dec71f44f98e715552eda3163f89982
DominicDev <s0l0estudi4r@gmail.com>
2026-05-11 18:34:15 +0200

    v0.0.3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import typegpuPlugin from 'unplugin-typegpu/vite';
 import tailwindVite from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/project-1379/',
   plugins: [
     tailwindVite(),
     typegpuPlugin(),
